d508568b68a882370d45d90feb7836cc80de705f
senzenn <ascenzen11@gmail.com>
2024-10-26 18:06:35 +0530

    halix

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,89 +2,89 @@ import express from "express";
 import { body, validationResult } from "express-validator";
 
 const app = express();
-const port = 5000;
 app.use(express.json());
+const PORT = 5000;
 
-const token = "addeklkKKyhy28283";
-const db = [
+let db = [
   {
     user: "aditya",
-    password: "1234",
-    token: token,
-  },
-  {
-    user: "naruto",
-    password: "1234",
-    token: null,
-  },
-  {
-    user: "kin",
-    password: "1234",
-    token: null,
+    pass: "12345678",
+    token: "2349343kkaduakdae",
   },
 ];
 
-// [TODO: GET]
+//[TODO: GET]
 app.get("/", (req, res) => {
-  res.send("home page");
+  res.send({
+    msg: "hello home ",
+  });
 });
 
-// [TODO: Post]
+app.get("/api/view", (req, res) => {
+  res.send(db);
+});
+
+//[TODO: POST]
+
 app.post(
   "/api/signup",
-  body("user").notEmpty().withMessage("User field cannot be empty"),
-  body("password").notEmpty().withMessage("Password field cannot be empty"),
+  body("user").isEmpty().isLength({ max: 3, min: 10 }),
+  body("pass").isEmpty().isLength({ max: 8, min: 20 }),
   (req, res) => {
     const result = validationResult(req);
-    const { user, password } = req.body;
+    const { user, pass } = req.body;
 
-    if (!result.isEmpty()) {
-      return res.status(400).json({
-        msg: "Please enter valid input",
-        errors: result.array(),
+    if (result.isEmpty()) {
+      return res.send({
+        msg: "first conditional block ",
+        error: result.array(),
+      });
+    } else {
+      res.send({
+        msg: "user created ",
+        name: user,
+        password: pass,
+      });
+
+      db.push({
+        user: user,
+        pass: pass,
       });
     }
-
-    db.push({ user: user, password: password, token: token });
-    return res.status(200).json({
-      msg: "User created",
-      userName: user,
-      password: password,
-    });
   },
 );
 
 app.post(
   "/api/login",
-  body("user").notEmpty().withMessage("User field cannot be empty"), // Validate user input
-  body("pass").notEmpty().withMessage("Password field cannot be empty"), // Validate password input
+  body("user").isEmpty(),
+
+  body("pass").isEmpty(),
   (req, res) => {
     const result = validationResult(req);
     const { user, pass } = req.body;
 
-    // Check for validation errors first
-    if (!result.isEmpty()) {
-      return res.status(400).json({
-        msg: "Fields can't be empty",
-        errors: result.array(),
-      });
-    }
-
-    const findUser = db.find((us) => us.user === user && us.password === pass);
-    if (findUser) {
-      return res.json({
-        msg: "User fetched successfully",
-        user: user,
-        token: token,
+    if (result.isEmpty()) {
+      return res.send({
+        error: result.array(),
       });
     } else {
-      return res.status(401).json({
-        msg: "User does not exist",
-      });
+      const findUser = db.find((u) => user == u.user && pass == u.pass);
+      if (findUser) {
+        return res.send({
+          msg: "user  fetched  ",
+          name: user,
+          password: pass,
+          token: "aidfjieijadkjfdaj433434",
+        });
+      } else {
+        res.json({
+          msg: "user does not exist",
+        });
+      }
     }
   },
 );
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`server is running on PORT ${PORT}`);
 });
---
+++ backend/src/high.js
@@ -0,0 +1,90 @@
+import express from "express";
+import { body, validationResult } from "express-validator";
+
+const app = express();
+const port = 5000;
+app.use(express.json());
+
+const token = "addeklkKKyhy28283";
+export const db = [
+  {
+    user: "aditya",
+    password: "1234",
+    token: token,
+  },
+  {
+    user: "naruto",
+    password: "1234",
+    token: null,
+  },
+  {
+    user: "kin",
+    password: "1234",
+    token: null,
+  },
+];
+
+// [TODO: GET]
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+// [TODO: Post]
+app.post(
+  "/api/signup",
+  body("user").notEmpty().withMessage("User field cannot be empty"),
+  body("password").notEmpty().withMessage("Password field cannot be empty"),
+  (req, res) => {
+    const result = validationResult(req);
+    const { user, password } = req.body;
+
+    if (!result.isEmpty()) {
+      return res.status(400).json({
+        msg: "Please enter valid input",
+        errors: result.array(),
+      });
+    }
+
+    db.push({ user: user, password: password, token: token });
+    return res.status(200).json({
+      msg: "User created",
+      userName: user,
+      password: password,
+    });
+  },
+);
+
+app.post(
+  "/api/login",
+  body("user").notEmpty().withMessage("User field cannot be empty"), // Validate user input
+  body("pass").notEmpty().withMessage("Password field cannot be empty"), // Validate password input
+  (req, res) => {
+    const result = validationResult(req);
+    const { user, pass } = req.body;
+
+    // Check for validation errors first
+    if (!result.isEmpty()) {
+      return res.status(400).json({
+        msg: "Fields can't be empty",
+        errors: result.array(),
+      });
+    }
+
+    const findUser = db.find((us) => us.user === user && us.password === pass);
+    if (findUser) {
+      return res.json({
+        msg: "User fetched successfully",
+        user: user,
+        token: token,
+      });
+    } else {
+      return res.status(401).json({
+        msg: "User does not exist",
+      });
+    }
+  },
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
